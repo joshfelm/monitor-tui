a7f3123203125454afc6401058d188ac597ff1ef
Joshua Felmeden <josh.felmeden@outlook.com>
2024-12-27 13:18:59 +0000

    Debug: add dummy xrandr data

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
 extern crate num_derive;
 extern crate num_traits;
 
+mod debug;
+
+use crate::debug::xrandr_debug::XRANDR_OUTPUT;
+
 use num_traits::FromPrimitive;
 use std::io;
 use std::process::Command;
@@ -55,9 +59,9 @@ impl App {
 #[derive(Clone, PartialEq)]
 struct Monitor {
     name: String,
-    resolution: (i32, i32),
-    displayed_resolution: (i32, i32),
-    available_resolutions: HashMap<(i32, i32), Vec<f32>>,  // Resolutions with vector of framerates
+    resolution: (i32, i32),                                 // Selected resolution
+    displayed_resolution: (i32, i32),                       // Resolution used may be different due to scale
+    available_resolutions: HashMap<(i32, i32), Vec<f32>>,   // Resolutions with vector of framerates
     scale: f32,
     framerate: f32,
     position: (i32, i32),
@@ -160,8 +164,10 @@ fn main() -> Result<(), io::Error> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
+    let debug = true;
+
     // Get monitor information
-    let monitors = get_monitor_info()?;
+    let monitors = get_monitor_info(debug)?;
 
     // Run the main loop
     let res = ui(&mut terminal, monitors);
@@ -275,8 +281,8 @@ fn vert_push(monitors: &mut Vec<Monitor>, pivot_monitor: usize, dir: Dir, vert_d
             shift_mons(monitors, pivot_monitor, difference, true, Vec::new());
         }
         monitors[app.selected_monitor].position = (monitors[pivot_monitor].position.0, monitors[pivot_monitor].position.1 - monitors[app.selected_monitor].resolution.1);
-        monitor_proximity(monitors);
     }
+    monitor_proximity(monitors);
 }
 
 fn horizontal_push(monitors: &mut Vec<Monitor>, pivot_monitor: usize, dir: Dir, vert_dir: Dir, app:App) {
@@ -1103,19 +1109,24 @@ fn monitor_proximity(monitors: &mut Vec<Monitor>) {
 }
 
 // get initial monitor information from xrandr
-fn get_monitor_info() -> io::Result<Vec<Monitor>> {
+fn get_monitor_info(debug: bool) -> io::Result<Vec<Monitor>> {
     let output = Command::new("xrandr")
         .arg("--query")
         .output()?;
-
     let stdout = String::from_utf8_lossy(&output.stdout);
+    let mut xrandr_lines = stdout.lines();
+
+    if debug {
+        xrandr_lines = XRANDR_OUTPUT.lines();
+    }
+
     let mut selected_framerate = 0.0;
     let mut selected_resolution = (0, 0);
     let mut monitors: Vec<Monitor> = Vec::new();
     let mut current_monitor: Option<Monitor> = None;
     let mut current_resolutions: HashMap<(i32, i32), Vec<f32>> = HashMap::new();  // HashMap to store resolutions and their framerates
 
-    for line in stdout.lines() {
+    for line in xrandr_lines {
         // include ends with mm to make sure we only get monitors that are connected AND in use
         if line.contains(" connected") && line.ends_with("mm") {
             // Push the previous monitor to the list if there was one
