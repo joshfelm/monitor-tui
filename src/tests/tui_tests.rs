d2d98968f76b9069579e98b3a5a4de1ccfab4bd0
Joshua Felmeden <josh.felmeden@outlook.com>
2025-02-07 12:56:55 +0000

    tests: separate big tests into smaller ones

--- src/tests/tui_tests.rs
+++ src/tests/tui_tests.rs
@@ -6,8 +6,8 @@ mod tests {
     use crate::xrandr::*;
     use crate::tui::*;
 
-    #[test]
     // test menu navigation
+    #[test]
     fn test_update_menu_left() {
         let mut app = App::new(State::MonitorEdit, true);
         let mut monitors = get_monitor_info(true).unwrap();
@@ -19,7 +19,6 @@ mod tests {
         handle_key_press(KeyCode::Char('l'), &mut monitors, &mut app);
 
         assert_eq!(app.selected_idx, 1);
-
     }
 
     #[test]
@@ -35,47 +34,107 @@ mod tests {
     }
 
     #[test]
-    fn test_states() {
-        let mut app = App::new(State::MonitorEdit, true);
+    fn test_starting_state() {
+        let app = App::new(State::MonitorEdit, true);
         let mut monitors = get_monitor_info(true).unwrap();
         monitor_proximity(&mut monitors);
 
         assert_eq!(app.state, State::MonitorEdit);
+    }
+
+    #[test]
+    fn m_key_sets_monitor_swap_state() {
+        let mut app = App::new(State::MonitorEdit, true);
+        let mut monitors = get_monitor_info(true).unwrap();
+        monitor_proximity(&mut monitors);
         handle_key_press(KeyCode::Char('m'), &mut monitors, &mut app);
         assert_eq!(app.state, State::MonitorSwap);
+    }
+
+    #[test]
+    fn enter_in_monitor_swap_returns_to_monitor_edit_state() {
+        let mut app = App::new(State::MonitorEdit, true);
+        let mut monitors = get_monitor_info(true).unwrap();
+        handle_key_press(KeyCode::Char('m'), &mut monitors, &mut app);
         handle_key_press(KeyCode::Enter, &mut monitors, &mut app);
         assert_eq!(app.state, State::MonitorEdit);
+    }
+
+    #[test]
+    fn select_monitor_sets_menu_select_state() {
+        let mut app = App::new(State::MonitorEdit, true);
+        let mut monitors = get_monitor_info(true).unwrap();
         handle_key_press(KeyCode::Enter, &mut monitors, &mut app);
         assert_eq!(app.state, State::MenuSelect);
+    }
 
+    #[test]
+    fn navigate_monitor_menu_to_resolution() {
+        let mut app = App::new(State::MonitorEdit, true);
+        let mut monitors = get_monitor_info(true).unwrap();
+        handle_key_press(KeyCode::Enter, &mut monitors, &mut app);
         // navigate to resolutions
         handle_key_press(KeyCode::Char('j'), &mut monitors, &mut app);
+        assert_eq!(app.state, State::MenuSelect);
         assert_eq!(app.menu_entry, MenuEntry::Resolution);
+    }
 
+    #[test]
+    fn navigate_to_info_edit_state() {
+        let mut app = App::new(State::MonitorEdit, true);
+        let mut monitors = get_monitor_info(true).unwrap();
+        handle_key_press(KeyCode::Enter, &mut monitors, &mut app);
+        // navigate to resolutions
+        handle_key_press(KeyCode::Char('j'), &mut monitors, &mut app);
         handle_key_press(KeyCode::Enter, &mut monitors, &mut app);
         assert_eq!(app.state, State::InfoEdit);
+    }
+
+    #[test]
+    fn debug_popup() {
+        let mut app = App::new(State::MonitorEdit, true);
+        let mut monitors = get_monitor_info(true).unwrap();
         handle_key_press(KeyCode::Char('d'), &mut monitors, &mut app);
         assert_eq!(app.state, State::DebugPopup);
+    }
 
-        // test escape is working
+    #[test]
+    fn esc_from_debug() {
+        let mut app = App::new(State::MonitorEdit, true);
+        let mut monitors = get_monitor_info(true).unwrap();
+        app.update_state(State::InfoEdit);
+        handle_key_press(KeyCode::Char('d'), &mut monitors, &mut app);
         handle_key_press(KeyCode::Esc, &mut monitors, &mut app);
         assert_eq!(app.state, State::InfoEdit);
+    }
+
+    #[test]
+    fn esc_from_info_edit() {
+        let mut app = App::new(State::MonitorEdit, true);
+        let mut monitors = get_monitor_info(true).unwrap();
+        app.update_state(State::InfoEdit);
         handle_key_press(KeyCode::Esc, &mut monitors, &mut app);
         assert_eq!(app.state, State::MenuSelect);
+    }
+
+    #[test]
+    fn debug_on_qmark() {
+        let mut app = App::new(State::MonitorEdit, true);
+        let mut monitors = get_monitor_info(true).unwrap();
         handle_key_press(KeyCode::Char('?'), &mut monitors, &mut app);
         assert_eq!(app.state, State::HelpPopup);
-        handle_key_press(KeyCode::Esc, &mut monitors, &mut app);
-        assert_eq!(app.state, State::MenuSelect);
-        handle_key_press(KeyCode::Esc, &mut monitors, &mut app);
-        assert_eq!(app.state, State::MonitorEdit);
+    }
 
-        // test quit
+    #[test]
+    fn quit_on_q() {
+        let mut app = App::new(State::MonitorEdit, true);
+        let mut monitors = get_monitor_info(true).unwrap();
         handle_key_press(KeyCode::Char('q'), &mut monitors, &mut app);
         assert_eq!(app.state, State::Quit);
     }
 
     #[test]
-    fn test_menu_navigation() {
+    fn test_menu_underflow() {
         let mut app = App::new(State::MenuSelect, true);
         let mut monitors = get_monitor_info(true).unwrap();
         monitor_proximity(&mut monitors);
@@ -85,6 +144,32 @@ mod tests {
         // make sure we don't underflow
         handle_key_press(KeyCode::Char('k'), &mut monitors, &mut app);
         assert_eq!(app.menu_entry, MenuEntry::Name);
+    }
+
+    #[test]
+    fn test_menu_overflow() {
+        let mut app = App::new(State::MenuSelect, true);
+        let mut monitors = get_monitor_info(true).unwrap();
+        monitor_proximity(&mut monitors);
+
+        assert_eq!(app.menu_entry, MenuEntry::Name);
+
+        app.menu_entry = MenuEntry::Resolutions;
+
+        // make sure we don't overflow
+        handle_key_press(KeyCode::Char('j'), &mut monitors, &mut app);
+        assert_eq!(app.menu_entry, MenuEntry::Resolutions);
+    }
+
+    #[test]
+    fn test_full_menu_navigation() {
+        let mut app = App::new(State::MenuSelect, true);
+        let mut monitors = get_monitor_info(true).unwrap();
+        monitor_proximity(&mut monitors);
+
+        assert_eq!(app.menu_entry, MenuEntry::Name);
+
+        // make sure we don't underflow
         handle_key_press(KeyCode::Char('j'), &mut monitors, &mut app);
         assert_eq!(app.menu_entry, MenuEntry::Resolution);
         handle_key_press(KeyCode::Char('j'), &mut monitors, &mut app);
@@ -103,10 +188,6 @@ mod tests {
         assert_eq!(app.menu_entry, MenuEntry::Up);
         handle_key_press(KeyCode::Char('j'), &mut monitors, &mut app);
         assert_eq!(app.menu_entry, MenuEntry::Right);
-        handle_key_press(KeyCode::Char('j'), &mut monitors, &mut app);
-        assert_eq!(app.menu_entry, MenuEntry::Resolutions);
-
-        // make sure we don't overflow
         handle_key_press(KeyCode::Char('j'), &mut monitors, &mut app);
         assert_eq!(app.menu_entry, MenuEntry::Resolutions);
 
